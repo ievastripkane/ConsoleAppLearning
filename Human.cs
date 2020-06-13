cc40b6e543b047f4378cb53cf4a3cd709db68c19
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-06-13 17:32:27 +0300

    Add project files.

---
+++ Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppLearning
+{
+    class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string EyeColor { get; set; }
+
+        public Human()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleAppLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person;
+            person = new Human();
+            person.Name = "Ieva";
+            person.Surname = "Stripkāne";
+            person.Age = 18;
+            person.EyeColor = "Blue";
+
+            Console.WriteLine(person.Name + " " + person.Surname);
+
+            person.Name = "Anna";
+            Console.WriteLine(person.Name + " " + person.Surname);
+        }
+    
+    }
+
+  
+
+
+
+
+
+
+
+}
+
